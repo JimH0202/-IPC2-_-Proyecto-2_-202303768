63a84ba83f997500805564b8bc7e18998049f815
JimH0202 <odemhurtarte@gmail.com>
2026-04-06 15:26:05 -0600

    respaldo

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c8f47b7199536341a271a70b261453445858419")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93a00b3baee0a65f00d971f8b657badeee48e42d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Frontend/obj/Debug/net10.0/Frontend.AssemblyInfo.cs
+++ Frontend/obj/Debug/net10.0/Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c8f47b7199536341a271a70b261453445858419")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93a00b3baee0a65f00d971f8b657badeee48e42d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
